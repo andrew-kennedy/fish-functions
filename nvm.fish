c2f79e87785329ec3ff51f9637a0297c5592155b
Andrew Kennedy <andrewk36@gmail.com>
2016-10-17 01:21:19 -0700

    tried to add nvm upgrade

--- nvm.fish
+++ nvm.fish
@@ -1,3 +1,10 @@
 function nvm
-	bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
+	bass source ~/.nvm/nvm.sh --no-use
+  if ($argv[1] == "upgrade")
+    set PAST_VERSION (nvm current)
+    nvm install $argv[2]
+    nvm use $argv[2]
+    nvm reinstall-packages $PAST_VERSION
+  else
+    nvm $argv
 end
