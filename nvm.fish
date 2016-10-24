4f7c34c7ccbbc22777dcc3e23146217c397818c2
Andrew Kennedy <andrewk36@gmail.com>
2016-10-24 04:44:31 -0700

    nvm fixed

--- nvm.fish
+++ nvm.fish
@@ -1,11 +1,12 @@
 function nvm
-	bass source ~/.nvm/nvm.sh --no-use
-  if ($argv[1] == "upgrade")
-    set PAST_VERSION (nvm current)
-    nvm install $argv[2]
-    nvm use $argv[2]
-    nvm reinstall-packages $PAST_VERSION
-  else
+
+#  if [ $argv[1] = "upgrade" ] ; and [ $argv[2] ]
+#    set PAST_VERSION (bass source ~/.nvm/nvm.sh --no-use ';' nvm current)
+#    bass source ~/.nvm/nvm.sh --no-use ';' \
+#    nvm install $argv[2] --reinstall-packages=$PAST_VERSION ';' \
+#    nvm use $argv[2] ';' \
+#  else
+    bass source ~/.nvm/nvm.sh --no-use ';' \
     nvm $argv
-  end
+#  end
 end
